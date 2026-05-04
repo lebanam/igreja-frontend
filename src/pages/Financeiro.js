f7c3e44fb308cd8fb50ac8706257238afd27c0c9
Lebana <lebanacms@gmail.com>
2026-05-04 12:48:07 -0300

    corrige financeiro

--- src/pages/Financeiro.js
+++ src/pages/Financeiro.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { DollarSign, TrendingUp, TrendingDown, BarChart3 } from "lucide-react";
+import { TrendingUp, TrendingDown, BarChart3 } from "lucide-react";
 import "./Celulas.css";
 
 function Financeiro() {
